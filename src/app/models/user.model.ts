71e26fad811b37161e51f929d8c73349a0be3d04
Beny.K <Beku1997@gmail.com>
2022-01-03 22:29:20 +0200

    s

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -3,16 +3,16 @@ import { Pet } from './pet.model';
 export interface User {
   _id?: string;
   fullname: string;
-  gender: string;
-  birthdate: string;
+  gender?: string;
+  birthdate?: string;
   email: string;
   password: string;
   location: {
-    address: string;
-    lat: string;
-    lng: string;
+    address?: string;
+    lat?: string;
+    lng?: string;
   };
-  imgURLs: Array<string>;
+  imgURLs?: Array<string>;
   joinedAt: string;
   prefs?: {
     age: string;
