f8fae551e75b09e7f566935903f45287305dbe77
Daniel Radia <danielradia21@gmail.com>
2022-01-03 11:52:29 +0200

    basic signup login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { AppLoginComponent } from './cmps/app-login/app-login.component';
 import { AppHomeComponent } from './pages/app-home/app-home.component';
-
+import { SignupComponent } from './pages/signup/signup.component';
+import { LoginComponent } from './pages/login/login.component';
 const routes: Routes = [
- 
   { path: '', component: AppHomeComponent },
-    { path: 'login', component: AppLoginComponent },
-
+  { path: 'signup', component: SignupComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { 
-  
-}
+export class AppRoutingModule {}
