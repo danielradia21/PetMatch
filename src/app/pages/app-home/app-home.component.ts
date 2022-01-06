83ccd33210011e0dc70a86fb9b0e3752aab1f573
Ilya <khirsah2211@gmail.com>
2022-01-06 16:15:23 +0200

    'addedPetCmp'

--- src/app/pages/app-home/app-home.component.ts
+++ src/app/pages/app-home/app-home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { AddPetComponent } from 'src/app/cmps/add-pet/add-pet/add-pet.component';
 @Component({
   selector: 'app-home',
   templateUrl: './app-home.component.html',
